46302b040792be32f8fa95bc86c6d79e10357b20
dvirbox <dvirbox@gmail.com>
2018-11-10 21:23:24 +0200

    resolve Task ,did not added tests yet
    problem :
    create table of code elements include view component.
    
    solution:
    1. added resolve function for each type of code.
    2. update view to present table as clear as posible.
    3. add tests - did not finish yet. test will be done in the next iteration

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,69 @@
+/* eslint-disable max-lines-per-function,complexity */
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
+import {resolveElements} from './code-analyzer';
 
 $(document).ready(function () {
+
     $('#codeSubmissionButton').click(() => {
+
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        let resolvedCode = resolveElements(parsedCode); /*this is the module!*/
+        var html =
+            '<table border=1 id="result" width="600">' +
+                '<col width="60">' +
+                '<col width="180">' +
+                '<tr>' +
+                    '<th>Line</th>' +
+                    '<th>Type</th>' +
+                    '<th>Name</th>' +
+                    '<th>Condition</th>' +
+                    '<th>Value</th>' +
+                '</tr>';
+        for (var i = 0; i < resolvedCode.length; i++) {
+            const codeElement = resolvedCode[i];
+            html += generateRow(codeElement.line, codeElement.type, codeElement.name, codeElement.condition, codeElement.value);
+        }
+        html+='</table>' + generateStyle();
+        document.getElementById('myTable').innerHTML = html;
     });
 });
+function generateRow(line,type,name,cond,val){
+    return `<tr><td>${line}</td><td>${type}</td><td>${name}</td><td>${cond}</td><td>${val}</td></tr>`;
+}
+function generateStyle(){
+    return `<style>
+        #result {
+            font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #result td, #result th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #result tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #result tr:hover {
+            background-color: #ddd;
+        }
+        #result tr td:nth-child(1),
+        tr td:nth-child(3),
+        tr td:nth-child(4),
+        tr td:nth-child(5)
+         { /* I don't think they are 0 based */
+            text-align: center;
+        }
+        #result th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: center;
+            background-color: #008faf;
+            color: white;
+        }</style>`;
+}
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,8 +1,151 @@
+/* eslint-disable no-case-declarations,no-console,complexity,max-lines-per-function */
 import * as esprima from 'esprima';
 
+export {parseCode, resolveElements};
+
 const parseCode = (codeToParse) => {
-    console.log(esprima.parseScript(codeToParse));
-    return esprima.parseScript(codeToParse);
+    console.log(esprima.parseScript(codeToParse, { loc: true }));
+    return esprima.parseScript(codeToParse,{ loc: true });
 };
 
-export {parseCode};
+// const parseCode = (codeToParse) => {
+//     console.log(esprima.parseScript(codeToParse, { loc: true }, function (node) {
+//         return node.type;
+//     }));
+//     esprima.parseScript(codeToParse,{ loc: true }, function (node) {
+//         toRet.push({type: node.type,line: node.loc.start.line, name: node.id? node.id.name : ''});
+//     });
+//     return toRet;
+// };
+
+function generateResolvedElement(line, type, name='', condition='', value=''){
+    return {line: line, type: type, name: name, condition: condition, value: value};
+}
+
+function resolveFunction(parsedFunction) {
+    const line = parsedFunction.loc.start.line;
+    const type ='FunctionDeclaration';
+    const name = resolveElements(parsedFunction.id);
+    const functionDeclaration = generateResolvedElement(line, type, name);//{line: line, type: 'FunctionDeclaration', name: name};
+    const paramRes = resolveParams(parsedFunction.params);
+    const bodyRes = resolveElements(parsedFunction.body);
+    return [].concat(functionDeclaration,paramRes,bodyRes);
+}
+
+function resolveParams(params) {
+    return params.map((param) => {
+        const line = param.loc.start.line;
+        const type = 'VariableDeclaration';
+        const name = resolveElements(param);
+        return generateResolvedElement(line, type, name);});//{line: param.loc.start.line, type: 'VariableDeclaration', name: param.name}));
+}
+
+function resolveBlockStatement(parsedBlockStatements) {
+    const reducer = (acc, statement) => acc.concat(resolveElements(statement));
+    return parsedBlockStatements.reduce(reducer,[]);
+}
+
+function resolveVariableDeclaration(parsedDeclarations) {
+    return parsedDeclarations.map((declaration) => {
+        const line = declaration.loc.start.line;
+        const type = 'VariableDeclaration';
+        const name = resolveElements(declaration.id);
+        return generateResolvedElement(line, type, name);});//({line: declaration.loc.start.line, type: 'VariableDeclaration', name: resolveElements(declaration.id)}));
+}
+
+function resolveAssignmentExpression(parsedAssignmentExpression) {
+    const line = parsedAssignmentExpression.loc.start.line;
+    const type = 'AssignmentExpression';
+    const name = resolveElements(parsedAssignmentExpression.left);
+    const value = resolveElements(parsedAssignmentExpression.right);
+    return generateResolvedElement(line, type, name, undefined, value);//{line: line, type: 'AssignmentExpression', name: name, value: value};
+}
+
+function resolveBinaryExpression(parsedBinaryExpression) {
+    const operator = parsedBinaryExpression.operator ;
+    const right = resolveElements(parsedBinaryExpression.right);
+    const left = resolveElements(parsedBinaryExpression.left);
+    return '' + left + ' ' + operator + ' ' + right;
+}
+
+function resolveWhileStatement(parsedWhileStatement) {
+    const line = parsedWhileStatement.loc.start.line;
+    const type = 'WhileStatement';
+    const test = resolveBinaryExpression(parsedWhileStatement.test);
+    const whileStatement = generateResolvedElement(line, type, undefined, test);//{line: parsedWhileStatement.loc.start.line, type: 'WhileStatement',cond: test};
+    const body = resolveElements(parsedWhileStatement.body);
+    return [].concat(whileStatement,body);
+}
+
+function resolveIfStatement(parsedIfStatement, isElseIf) {
+    const generateIfType = (isElseIf) => isElseIf ? 'ElseIfStatement' : 'IfStatement';
+    const line = parsedIfStatement.loc.start.line;
+    const type = generateIfType(isElseIf);
+    const test = resolveBinaryExpression(parsedIfStatement.test);
+    const ifStatement = generateResolvedElement(line, type, undefined,test);//{line: parsedIfStatement.loc.start.line, type: generateIfType(isElseIf), cond: test};
+    const body = resolveElements(parsedIfStatement.consequent);
+    const alternate = (parsedIfStatement.alternate.type === 'IfStatement') ?
+        resolveIfStatement(parsedIfStatement.alternate, /*isElseIf*/true) :
+        resolveElements(parsedIfStatement.alternate);
+    return [].concat(ifStatement,body,alternate);
+}
+
+function resolveReturnStatement(parsedReturnStatement) {
+    const line = parsedReturnStatement.loc.start.line;
+    const value = resolveElements(parsedReturnStatement.argument);
+    const type ='ReturnStatement';
+    return generateResolvedElement(line, type, undefined, undefined,value);//{line: line, type: 'ReturnStatement', value: value};
+}
+
+function resolveIdentifier(parsedIdentifier) {
+    return parsedIdentifier.name;
+}
+
+function resolveLiteral(parsedLiteral) {
+    return parsedLiteral.value;
+}
+
+function resolveMemberExpression(parsedMemberExpression) {
+    const object = resolveElements(parsedMemberExpression.object);
+    const property = resolveElements(parsedMemberExpression.property);
+    return object + '[' + property + ']';
+}
+
+function resolveUnaryExpression(parsedUnaryExpression) {
+    const value = resolveElements(parsedUnaryExpression.argument);
+    const operator = parsedUnaryExpression.operator;
+    return operator + value;
+}
+
+function resolveElements(parsedCode){
+    switch (parsedCode.type) {
+        case 'Program':
+            return resolveElements(parsedCode.body[0]);
+        case 'FunctionDeclaration':
+            return resolveFunction(parsedCode);
+        case 'BlockStatement':
+            return resolveBlockStatement(parsedCode.body);
+        case 'VariableDeclaration':
+            return resolveVariableDeclaration(parsedCode.declarations);
+        case 'ExpressionStatement':
+            return resolveElements(parsedCode.expression);
+        case 'AssignmentExpression':
+            return resolveAssignmentExpression(parsedCode);
+        case 'WhileStatement':
+            return resolveWhileStatement(parsedCode);
+        case 'IfStatement':
+            return resolveIfStatement(parsedCode, /*isElseIf*/false);
+        case 'ReturnStatement':
+            return resolveReturnStatement(parsedCode);
+        case 'Identifier':
+            return resolveIdentifier(parsedCode);
+        case 'Literal':
+            return resolveLiteral(parsedCode);
+        case 'BinaryExpression':
+            return resolveBinaryExpression(parsedCode);
+        case 'MemberExpression':
+            return resolveMemberExpression(parsedCode);
+        case 'UnaryExpression':
+            return resolveUnaryExpression(parsedCode);
+    }
+}
